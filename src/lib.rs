2bda2f34434e664d9f96bcb99e81db8eb8faa015
Oleg Klimenko <me@ctizen.dev>
2026-05-13 23:21:12 +0300

    update package

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![forbid(unsafe_code)]
-
-use mahjong_seatings_rust::{make_interval_seating, make_shuffled_seating, make_swiss_seating, PlayersMap};
+extern crate web_sys;
+use mahjong_seatings_rust::{
+    make_interval_seating, make_shuffled_seating, make_swiss_seating, PlayersMap, WindShuffle,
+};
 use serde::{Deserialize, Serialize};
 use serde_wasm_bindgen::{from_value, to_value};
 use wasm_bindgen::prelude::*;
@@ -8,8 +10,10 @@ use wasm_bindgen::prelude::*;
 #[derive(Deserialize)]
 pub struct IntervalSeatingInput {
     pub players_map: PlayersMap,
+    pub previous_seatings: Vec<Vec<u32>>,
     pub step: usize,
     pub rand_factor: u64,
+    pub wind_shuffle: u8,
 }
 
 #[derive(Deserialize)]
@@ -18,6 +22,7 @@ pub struct ShuffledSeatingInput {
     pub previous_seatings: Vec<Vec<u32>>,
     pub groups_count: u32,
     pub rand_factor: u64,
+    pub wind_shuffle: u8,
 }
 
 #[derive(Deserialize)]
@@ -25,11 +30,21 @@ pub struct SwissSeatingInput {
     pub players_map: PlayersMap,
     pub previous_seatings: Vec<Vec<u32>>,
     pub rand_factor: u64,
+    pub wind_shuffle: u8,
 }
 
 #[derive(Serialize)]
 pub struct SeatingsCalcResult {
     pub result: PlayersMap,
+}
+
+fn input_to_wind_shuffle(wind_shuffle: u8) -> WindShuffle {
+    match wind_shuffle {
+        0 => WindShuffle::Random,
+        1 => WindShuffle::Balanced,
+        2 => WindShuffle::Prescripted,
+        _ => WindShuffle::Random,
+    }
 }
 
 #[wasm_bindgen]
@@ -39,8 +54,15 @@ pub fn make_seating_shuffled(val: JsValue) -> JsValue {
     match i {
         Ok(i) => {
             let result = SeatingsCalcResult {
-                result: make_shuffled_seating(&i.players_map, &i.previous_seatings, i.groups_count, i.rand_factor)
+                result: make_shuffled_seating(
+                    &i.players_map,
+                    &i.previous_seatings,
+                    i.groups_count,
+                    i.rand_factor,
+                    input_to_wind_shuffle(i.wind_shuffle),
+                ),
             };
+
             to_value(&result).unwrap()
         }
         Err(e) => to_value(&e.to_string()).unwrap(),
@@ -54,7 +76,13 @@ pub fn make_seating_interval(val: JsValue) -> JsValue {
     match i {
         Ok(i) => {
             let result = SeatingsCalcResult {
-                result: make_interval_seating(&i.players_map, i.step, i.rand_factor)
+                result: make_interval_seating(
+                    &i.players_map,
+                    &i.previous_seatings,
+                    i.step,
+                    i.rand_factor,
+                    input_to_wind_shuffle(i.wind_shuffle),
+                ),
             };
             to_value(&result).unwrap()
         }
@@ -69,7 +97,12 @@ pub fn make_seating_swiss(val: JsValue) -> JsValue {
     match i {
         Ok(i) => {
             let result = SeatingsCalcResult {
-                result: make_swiss_seating(&i.players_map, &i.previous_seatings, i.rand_factor)
+                result: make_swiss_seating(
+                    &i.players_map,
+                    &i.previous_seatings,
+                    i.rand_factor,
+                    input_to_wind_shuffle(i.wind_shuffle),
+                ),
             };
             to_value(&result).unwrap()
         }
